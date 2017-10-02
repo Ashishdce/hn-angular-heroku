8429bc1c003d8f18a0ef246e7bbcbc81c6e6b06f
Ashish Jangid <ajangi@sapient.com>
2017-10-02 18:31:13 +0530

    added express as dependency

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ app.set('views', 'dist');
 app.get('*', angularRouter);
 
 app.listen(process.env.PORT || 8080, function() {
-    var port = server.address().port;
-    console.log("App now running on port", port);
+
+    console.log("App now running on port");
 });
